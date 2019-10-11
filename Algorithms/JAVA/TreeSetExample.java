9ad5c375a26a906e9df7a34c01192c4e7d167acf
lunodesouza <wender.andrade@inmetrics.com.br>
2019-10-11 10:36:56 -0300

    Added TreeSet use example.

---
+++ Algorithms/JAVA/TreeSetExample.java
@@ -0,0 +1,41 @@
+import java.util.TreeSet;
+
+/**
+* Example use of the java.util.TreeSet class
+*/
+public class TreeSetExample {
+        public static void main(String[] args) {
+                System.out.println("TreeSet is like HashSet but it sorts the elements in ascending order.");
+
+				// Instance using Default Constructor
+                TreeSet<String> treeSet = new TreeSet<String>(); 
+				// adding elements using the .add() method
+                treeSet.add("Simple");                                         
+                treeSet.add("TreeSet");
+                treeSet.add("TreeSet");
+
+				// output will be a set of strings in the instance.
+                System.out.println(treeSet);                                    
+
+                // TreeSet is implemented from the SortedSet interface and will omit duplicates
+                TreeSet<Integer> duplicateEntryTreeSet = new TreeSet<Integer>();
+                duplicateEntryTreeSet.add(300);
+                duplicateEntryTreeSet.add(102);
+				
+				// Duplicate value will not be stored
+                duplicateEntryTreeSet.add(102);                                 
+
+				// Output will be [102, 300]
+                System.out.println(duplicateEntryTreeSet);                      
+
+                // Objects stored in TreeSet should be homogeneous
+                // and comparable otherwise the program will throw an exception
+
+                TreeSet<StringBuilder> builderSet = new TreeSet<StringBuilder>();
+				// Will result in java.lang.ClassCastException
+                builderSet.add(new StringBuilder("T"));                         
+                builderSet.add(new StringBuilder("M"));
+                builderSet.add(new StringBuilder("I"));
+
+        }
+}
