033e4f726b4a5fc61a568c34f07830f84a9456c3
arti-kumari-0506 <56881542+arti-kumari-0506@users.noreply.github.com>
2019-10-22 22:37:41 +0530

    Create linsearch.c

---
+++ coding/linsearch.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+   int a[10],n,i,flag=0;
+   for(i=0;i<10;i++)
+   {
+      scanf("%d",&a[i]);
+    }
+    scanf("%d",&n);
+    for(i=0;i<10;i++)
+     {
+       if(a[i]==n)
+       flag=1;
+     }
+     if(flag==1)
+     printf("Element found");
+     else
+     printf("Element not found");
+     return 0;
+    }
