d01c03a64663f6835d07e1f62f7583d75b4c387c
Pranav Annam <pranavannam@gmail.com>
2019-10-29 14:07:58 +0530

    minor change

--- CharSum.c
+++ CharSum.c
@@ -2,6 +2,7 @@
 
 int main()
 {
+    printf("Enter the String of which you want to find the sum\n");
     char str[100]; /* Declares a string of size 100 */
     int t,sum=0,i=0;
     scanf("%s",str);
@@ -9,10 +10,10 @@ int main()
     {
       t+=str[i]-96;
     //  printf("%d",t);
-     
+
       i++;
     }
     printf("%d",t);
-  
-  return 0; 
+
+  return 0;
 }
